b6e19c13f13057a5eff5d9a7d4e1d8179e91cd02
Samuel Massé <samuelmasse4@gmail.com>
2022-03-25 18:47:08 -0400

    yarn 3

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import path from 'path'
 
 const app = express()
 
 app.use(express.json())
+app.use('/webchat', express.static(path.join(__dirname, '../node_modules/@botpress/webchat-inject/dist')))
 
 app.listen(3125)
 console.log(`listening on port 3125`)
